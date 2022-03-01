a538d82bf48fb472adcdd52a3bf76bff58c74623
VIVEK RAI <94076044+Vivekrai2098@users.noreply.github.com>
2022-03-01 14:25:34 +0530

    chatbox

---
+++ chatbox.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	//this is first path of typing
+clrscr();	
+char na[10];
+gets(na);
+printf("hello sir ");	
+//this is second part	
+char jai[10];
+scanf("%s",jai);
+gets(jai);
+printf(" how can i help you sir ");		
+//this is third part	
+char ho[10];
+scanf("%s",ho);
+gets(ho);
+printf("sir dont worry about it..we simply solved your problem");
+//this is second part	
+char jai[10];
+scanf("%s",jai);
+gets(jai);
+printf("we help you sir ");	
+//this is second part	
+char jai[10];
+scanf("%s",jai);
+gets(jai);
+printf("thanks sir ");	
+//this is second part	
+char jai[10];
+scanf("%s",jai);
+gets(jai);
+printf("give feed back");	
+//this is second part	
+char jai[10];
+scanf("%s",jai);
+gets(jai);
+printf("you are chat with chat box thanks ");		
+	
+getch();	
+}
